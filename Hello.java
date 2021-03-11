a164a16aa86c7abe1c8049b590a4af35a6f9d95a
CadeLilley <cadelilley@gmail.com>
2021-03-10 20:05:17 -0800

    Alterations to Hello.java from A

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Absolutely a new line that will require a merge. Not staging");
+    System.out.println("Alterations have been made from 'Server A'");
     System.out.println("Hello, World");
   }
 }
