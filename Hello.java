18e1a612febaff8926af8fa278764b3fc3af8f51
CadeLilley <cadelilley@gmail.com>
2021-03-10 20:47:05 -0800

    Fixed merging conflicts within Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
 class Hello {
   public static void main(String[] args) {
-<<<<<<< Updated upstream
     System.out.println("Alterations have been made from 'Server A'");
-=======
-    System.out.println("Line created in server B");
->>>>>>> Stashed changes
     System.out.println("Hello, World");
   }
 }
